a1dc6659aad2b4762de1200d6e26d7d711a404ac
Davide Galilei <davidegalilei2018@gmail.com>
2021-05-15 21:04:54 +0200

    Reformatting

--- gpytranslate/types/translated_object.py
+++ gpytranslate/types/translated_object.py
@@ -5,12 +5,18 @@ from typing import Union
 
 class TranslatedObject(dict):
     def __getattr__(self: Union[str, dict, list], attr: str):
-        if isinstance(self, list): return [TranslatedObject(elem) for elem in self]
-        obj = TranslatedObject(dict.get(self, attr)) if isinstance(dict.get(self, attr), dict) else dict.get(self, attr)
-        return obj
+        if isinstance(self, list):
+            return [TranslatedObject(elem) for elem in self]
+        return (
+            TranslatedObject(dict.get(self, attr))
+            if isinstance(dict.get(self, attr), dict)
+            else dict.get(self, attr)
+        )
 
     def __str__(self):
-        return json.dumps({k: v if len(str(v)) < 200 else "..." for k, v in self.items()}, indent=4)
+        return json.dumps(
+            {k: v if len(str(v)) < 200 else "..." for k, v in self.items()}, indent=4
+        )
 
     __setattr__ = dict.__setitem__
 
