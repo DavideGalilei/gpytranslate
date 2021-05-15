0cbe1e1428391aa8ae89a2968d152f5b8fb8ba2c
Davide Galilei <davidegalilei2018@gmail.com>
2021-05-15 21:06:20 +0200

    Major Update: 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gpytranslate",
-    version="0.3.1",
+    version="1.0.1",
     author="Davide Galilei",
     author_email="davidegalilei2018@gmail.com",
     description="A Python3 library for translating text using Google Translate API.",
@@ -18,8 +18,8 @@ setuptools.setup(
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=[
-   'httpx',
-    ]
+        "httpx",
+    ],
 )
