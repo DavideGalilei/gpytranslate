9d76bb1c1fb2d236432ddf60e70134a4d440ce9d
alsoGAMER <35269770+alsoGAMER@users.noreply.github.com>
2021-05-31 19:02:05 +0200

    Minor fixes

--- gpytranslate/gpytranslate.py
+++ gpytranslate/gpytranslate.py
@@ -15,6 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+
 import io
 from collections.abc import Mapping
 from typing import Union, Dict, List, Any
