11a17534780fdafc9002a51afab12b91e7210847
Davide Galilei <davidegalilei2018@gmail.com>
2021-05-31 17:32:12 +0200

    Added BASE_HEADERS to be sent along with the library requests

--- gpytranslate/types/__init__.py
+++ gpytranslate/types/__init__.py
@@ -1,2 +1,6 @@
 from .translated_object import TranslatedObject
 from .base_translator import BaseTranslator
+
+BASE_HEADERS: dict = {
+    "User-Agent": "GoogleTranslate/6.6.1.RC09.302039986 (Linux; U; Android 9; Redmi Note 8)",
+}
