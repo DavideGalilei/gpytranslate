0778a34f07a5be732e4ca388755f526149e8fc03
Davide Galilei <davidegalilei2018@gmail.com>
2024-12-03 20:02:54 +0100

    refactor: Remove license headers from multiple files

--- tests/async/test_translate.py
+++ tests/async/test_translate.py
@@ -1,50 +1,21 @@
-"""
-    gpytranslate - A Python3 library for translating text using Google Translate API.
-    MIT License
-
-    Copyright (c) 2023 Davide Galilei
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-"""
+from typing import Any, Dict, List
 
 import pytest
 
-from typing import List, Dict, Any
-
-from gpytranslate import Translator, TranslatedObject
+from gpytranslate import TranslatedObject, Translator
 
 
 @pytest.mark.asyncio
 async def test_translate_auto():
     translator = Translator()
-    translation: TranslatedObject = await translator.translate(
-        "Ciao Mondo.", targetlang="en"
-    )
+    translation: TranslatedObject = await translator.translate("Ciao Mondo.", targetlang="en")
     assert translation.text == "Hello World.", "Translations are not equal."
 
 
 @pytest.mark.asyncio
 async def test_translate_source():
     translator = Translator()
-    translation: TranslatedObject = await translator.translate(
-        "Ciao.", sourcelang="it", targetlang="en"
-    )
+    translation: TranslatedObject = await translator.translate("Ciao.", sourcelang="it", targetlang="en")
 
     assert translation.text in ("Hello.", "HI."), "Translations are not equal."
 
@@ -52,9 +23,7 @@ async def test_translate_source():
 @pytest.mark.asyncio
 async def test_translate_list():
     translator = Translator()
-    translations: List[TranslatedObject] = await translator.translate(
-        ["Ciao Mondo.", "Come stai?"], targetlang="en"
-    )
+    translations: List[TranslatedObject] = await translator.translate(["Ciao Mondo.", "Come stai?"], targetlang="en")
 
     assert [translation.text for translation in translations] == [
         "Hello World.",
--- tests/sync/test_sync_translate.py
+++ tests/sync/test_sync_translate.py
@@ -1,29 +1,4 @@
-"""
-    gpytranslate - A Python3 library for translating text using Google Translate API.
-    MIT License
-
-    Copyright (c) 2023 Davide Galilei
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-"""
-
-from typing import List, Dict, Any
+from typing import Any, Dict, List
 
 from gpytranslate import SyncTranslator, TranslatedObject
 
@@ -36,18 +11,14 @@ def test_sync_translate_auto():
 
 def test_sync_translate_source():
     translator = SyncTranslator()
-    translation: TranslatedObject = translator.translate(
-        "Ciao.", sourcelang="it", targetlang="en"
-    )
+    translation: TranslatedObject = translator.translate("Ciao.", sourcelang="it", targetlang="en")
 
     assert translation.text in ("Hello.", "HI."), "Translations are not equal."
 
 
 def test_sync_translate_list():
     translator = SyncTranslator()
-    translations: List[TranslatedObject] = translator.translate(
-        ["Ciao Mondo.", "Come stai?"], targetlang="en"
-    )
+    translations: List[TranslatedObject] = translator.translate(["Ciao Mondo.", "Come stai?"], targetlang="en")
 
     assert [translation.text for translation in translations] == [
         "Hello World.",
@@ -65,3 +36,10 @@ def test_sync_translate_dict():
         1: "Hello World.",
         2: "How are you?",
     }, "Translations are not equal."
+
+
+if __name__ == "__main__":
+    test_sync_translate_auto()
+    test_sync_translate_source()
+    test_sync_translate_list()
+    test_sync_translate_dict()
